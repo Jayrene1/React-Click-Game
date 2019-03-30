4a18ce5ee63d1ac0b623bf9b7189f29fd7c8a258
Jayrene1 <43413926+Jayrene1@users.noreply.github.com>
2019-03-29 23:13:09 -0400

    Updates

---
+++ precache-manifest.084e4000e9872366e327ce14f2818883.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ec4adcd389b3cafea1c",
+    "url": "/React-Click-Game/static/js/runtime~main.2be38ff2.js"
+  },
+  {
+    "revision": "8c1ca60c66b3a59bf9a0",
+    "url": "/React-Click-Game/static/js/main.7c8e9f78.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/React-Click-Game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "8c1ca60c66b3a59bf9a0",
+    "url": "/React-Click-Game/static/css/main.0ff6481a.chunk.css"
+  },
+  {
+    "revision": "e49d590e2ef3b723c620cc02cbdf539c",
+    "url": "/React-Click-Game/index.html"
+  }
+];
